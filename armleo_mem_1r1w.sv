370068a40760a162fa5287a5be92f4382410af70
Arman Avetisyan <arman.avetisyan2000@gmail.com>
2022-06-12 11:20:40 +0400

    Fixed mem 1r1w .core

--- armleo_mem_1r1w.sv
+++ armleo_mem_1r1w.sv
@@ -1,16 +1,18 @@
 ////////////////////////////////////////////////////////////////////////////////
 // 
 // Copyright (C) 2016-2021, Arman Avetisyan
-// 
-// Purpose:	Used by regfile to allow two reads and one write.
-//		two instances is used, where
+// Description: Memory inferrable Block RAM cell 1 sync read-first port,
+// 		1 sync write port w/o mask. Read port's output stays stable
+//		until next read.
+// Purpose:	Originally used by regfile to allow two reads and one write.
+//		Two instances is used, where
 //		write port is connected together and exposed and
 //		two read ports are exposed to core itself
 //		
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-module armleocpu_1r1w (clk, read_addr, read, read_data, write_addr, write, write_data);
+module armleo_mem_1r1w (clk, read_addr, read, read_data, write_addr, write, write_data);
 	parameter DEPTH_LOG2 = 5;
 	localparam ELEMENTS = 2**DEPTH_LOG2;
 	parameter WIDTH = 32;
